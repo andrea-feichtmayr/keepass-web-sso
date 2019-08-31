c89b4e716c5ba9e99bd1762df3f3df2544799583
Psych1cFury <49362696+Psych1cFury@users.noreply.github.com>
2019-08-31 11:48:42 +0200

    Initial commit

---
+++ KeePassWebSSO/KeePassWebSSOExt.cs
@@ -0,0 +1,13 @@
+﻿using KeePass.Plugins;
+
+namespace KeePassWebSSO {
+    public class KeePassWebSSOExt : Plugin {
+        private SSOWebRequestCreator m_requestCreator;
+
+        public override bool Initialize(IPluginHost host) {
+            this.m_requestCreator = new SSOWebRequestCreator();
+            this.m_requestCreator.Register();
+            return true;
+        }
+    }
+}
---
+++ KeePassWebSSO/SSOWebRequestCreator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Text.RegularExpressions;
+
+
+namespace KeePassWebSSO {
+    class SSOWebRequestCreator : IWebRequestCreate {
+        private static readonly string s_suffix = "+sso";
+        private static readonly string[] s_schemes = new string[] { "http", "https" };
+
+        public void Register() {
+            foreach (string scheme in s_schemes) {
+                WebRequest.RegisterPrefix($"{scheme}{s_suffix}:", this);
+            }
+        }
+
+        public WebRequest Create(Uri uri) {
+            UriBuilder builder = new UriBuilder(uri);
+            builder.Scheme = Regex.Replace(builder.Scheme, Regex.Escape(s_suffix) + "$", "");
+
+            WebRequest request = WebRequest.Create(builder.Uri);
+            request.Credentials = CredentialCache.DefaultCredentials;
+            return request;
+        }
+    }
+}
